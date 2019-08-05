8d1708f0fd11abc49a48355702fc8f173b658b6c
Amit <somuamit@gmail.com>
2019-08-05 12:38:50 +0530

    changes

---
+++ user-portal/src/main/java/com/angular/spring/userportal/UserPortalApplication.java
@@ -0,0 +1,13 @@
+package com.angular.spring.userportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserPortalApplication.class, args);
+	}
+
+}
